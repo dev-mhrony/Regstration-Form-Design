6ff017b4be421f423850fba1516c884d3c885f12
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-05-13 13:49:05 +0600

    last update

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration From</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+
+
+    <div class="wrapper">
+        <div class="main">
+            <h1>Create Account</h1>
+            <form method="post" action = "">
+            <p><input type="text" name = "fname" placeholder="Fast Name" class = "input"></p>
+            <p><input type="text" name = "lname" placeholder="Last Name" class = "input"></p>
+            <p><input type="email" name = "email" placeholder="Email" class = "input"></p>
+            <p><input type="number" name = "phone" placeholder="Phone Number" class = "input"></p>
+            <p><input type="password" name = "password" placeholder="Password" class = "input"></p>
+         
+
+            <div class="butn">
+                <p><input type = "Submit" name="sub" value = "Register" class = "btn fl"></p>
+            </div>
+            </form>
+        </div>
+    </div
+    </div
+    </div>
+    
+</body>
+</html>
---
+++ insert.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</body>
+</html>
